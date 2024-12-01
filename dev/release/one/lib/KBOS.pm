6aedd17b49c27da393afecc7b9a9ec5db1c86ef8
Lichtkind <lichtkind@cpan.org>
2024-12-01 17:19:52 +0100

    prepare dev, put old code into new name space

---
+++ dev/release/one/lib/KBOS.pm
@@ -0,0 +1,12 @@
+
+# exporting symbols
+
+package KBOS;
+use v5.20;
+use Exporter;
+use KBOS::Package;
+use KBOS::Closure;
+use KBOS::Data;
+use KBOS::Class;
+
+1;
---
+++ dev/release/one/lib/KBOS/Class.pm
@@ -0,0 +1,14 @@
+
+# root of OO system
+
+package KBOS::Class;
+our $VERSION = 0.2;
+use v5.20;
+use warnings;
+
+use KBOS::Data::Type qw/:all/;
+use Exporter 'import';
+#our @EXPORT_OK = ('clone_data', 'date_time', @KBOS::Data::Type::EXPORT_OK);
+#our %EXPORT_TAGS = (all => [@EXPORT_OK], type => [@KBOS::Data::Type::EXPORT_OK]);
+
+1;
